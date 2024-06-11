2cda2b52c30d31868d592e68f488c5766332e0bb
Roberto <robertofalcongonzalez@gmail.com>
2024-06-11 14:47:42 -0400

    Agregando paginas

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,13 +5,19 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
+import {createRouter, createWebHistory} from 'vue-router/auto'
+import {setupLayouts} from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   extendRoutes: (routes) => setupLayouts(routes),
 
+})
+router.beforeEach((to, from, next) => {
+  if (!localStorage.getItem('token')) {
+    next({path: 'login'})
+  }
+  return false
 })
 
 export default router
