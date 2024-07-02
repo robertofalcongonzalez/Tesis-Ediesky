f3c77d6851e58299e16c84ebbd0d9a5bd271d439
Roberto <robertofalcongonzalez@gmail.com>
2024-07-02 04:21:07 -0400

    Arreglando mensaje de error en login

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -3,13 +3,15 @@ import {defineStore} from 'pinia'
 
 export const useAppStore = defineStore('app', () => {
   const error = ref('')
+  const toLogin = ref(false)
   const snackBar = ref(false)
   const snackBarText = ref('')
-  const setError = (serverError: any) => {
+  const setError = (serverError: any, sentToLogin: boolean = false) => {
     error.value = serverError;
+    toLogin.value = sentToLogin;
     snackBar.value = true;
     snackBarText.value = error.value
   }
-  return {error, snackBar, snackBarText, setError}
+  return {error, snackBar,toLogin, snackBarText, setError}
 })
 
